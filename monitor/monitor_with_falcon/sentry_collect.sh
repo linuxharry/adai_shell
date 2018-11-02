da9a4f5151e82d007859de814538a27132d69896
lihefei <lihefei@chunyu.me>
2018-11-02 15:37:23 +0800

    fix

--- monitor/monitor_with_falcon/sentry_collect.sh
+++ monitor/monitor_with_falcon/sentry_collect.sh
@@ -6,11 +6,11 @@ for api in $apis;do
   ts=`date +%s`;
   t=`LC_ALL=en_US.UTF-8 date '+%d/%b/%Y:%H:%M' -d "-1 minute"`  # 每分钟采集一次日志
   if [ $api -eq 3 ];then
-    project=medweb_online
+    project=project1
   elif [ $api -eq 24 ];then
-    project=op
+    project=project2
   fi
-  valus=`tail -1000000 /usr/local/nginx/logs/access.log |egrep 'sentry.chunyu.me' |egrep $t |egrep 'POST'|egrep "/api/$api/" |wc -l`
+  valus=`tail -1000000 /usr/local/nginx/logs/access.log |egrep 'sentry.chunyu.club' |egrep $t |egrep 'POST'|egrep "/api/$api/" |wc -l`
   curl -X POST -d "[{\"metric\": \"sentry\", \"endpoint\": \"sentry\", \"timestamp\": $ts,\"step\": 60,\"value\": $valus,\"type\":\"sentry\",\"counterType\": \"GAUGE\",\"tags\": \"project=$project\"}]" http://127.0.0.1:1988/v1/push
 done
 # 注意："tail -1000000" 根据nginx访问量配置数值
